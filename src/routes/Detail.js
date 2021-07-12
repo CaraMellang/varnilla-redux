a548d553da58bee915394b40bd85f62415547aea
Mellang <dlwpgh23@naver.com>
2021-07-12 23:43:15 +0900

    toDos가 왜 undefined?

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
+// import { useParams } from "react-router-dom";
 
-const Detail = () => {
+const Detail = ({ toDos }) => {
+  // const id = useParams(); 리덕스 대신 useParams 써도 됨
+  // console.log(id);
+  console.log(toDos);
   return (
     <div>
       <h1>elxpdlf</h1>
@@ -8,4 +13,14 @@ const Detail = () => {
   );
 };
 
-export default Detail;
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  console.log(id);
+  return { toDos: state.find((toDo) => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
